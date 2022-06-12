01ef3b146026a839af8302b6c7766f411cee6d25
Amit Singh Dalal <ad@webscoot.io>
2022-06-12 22:08:36 +0530

    mangum

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
-from api.v1.api import router as api_router
+# from api.v1.api import router as api_router
 
 app = FastAPI(title='Serverless Lambda FastAPI')
 
-app.include_router(api_router, prefix="/api/v1")
+# app.include_router(api_router, prefix="/api/v1")
 
 
 @app.get("/",  tags=["Endpoint Test"])
